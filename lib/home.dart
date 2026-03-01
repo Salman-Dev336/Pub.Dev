353bb399928f92b97c76bc033a99df73f04c38d5
Salman Khan <salmantechhub336@gmail.com>
2026-03-02 03:13:32 +0500

    animated text packages and practice on it

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:badges/badges.dart' as badges;
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -29,6 +32,47 @@ class _HomeState extends State<Home> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Row(
+  mainAxisSize: MainAxisSize.min,
+  children: <Widget>[
+    const SizedBox(width: 20.0, height: 100.0),
+    const Text(
+      'Be',
+      style: TextStyle(fontSize: 43.0,
+      color: Colors.deepPurpleAccent),
+    ),
+    const SizedBox(width: 20.0, height: 100.0),
+    DefaultTextStyle(
+      style: const TextStyle(
+        fontSize: 40.0,
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Horizon',
+        color: Colors.greenAccent,
+      ),
+      child: AnimatedTextKit(
+        animatedTexts: [
+          RotateAnimatedText('AWESOME'),
+          RotateAnimatedText('OPTIMISTIC'),
+          RotateAnimatedText('DIFFERENT'),
+        ],
+        
+        onTap: () {
+        },
+      ),
+    ),
+  ],
+),
+          // AnimatedTextKit(animatedTexts: [
+          //   TyperAnimatedText(
+          //     "Salman khan flutter developer",
+          //     textStyle: TextStyle(
+          //       fontSize: 50,
+          //       color: Colors.deepPurpleAccent,
+          //       fontFamily: 'canterbury',
+          //       )
+          //   )
+          // ],
+          // ),
         //  Center(
         //    child: badges.Badge(
         //     badgeContent: Text('3'),
